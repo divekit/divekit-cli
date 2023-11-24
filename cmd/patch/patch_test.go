26e8f3ee2232fcdbe8e02c0e2220bbb72ad59579
thomasdonst <thomas.donst@gmx.de>
2023-11-24 23:09:55 +0100

    added build package to create installer files for CD

---
+++ cmd/patch/patch_test.go
@@ -0,0 +1,35 @@
+package patch
+
+import (
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	// todo
+	//patch = &cobra.Command{Use: "root", RunE: run, PreRunE: preRun}
+	//RootCmdFlags(patch)
+	//
+	//err := patch.Execute()
+	//
+	//t.Errorf("error: %v", err)
+}
+
+func TestPreRun(t *testing.T) {
+	// todo
+	//patch = &cobra.Command{Use: "root", RunE: PreRun}
+}
+
+func TestDefinePatchFiles(t *testing.T) {
+	// todo
+	//definePatchFiles()
+}
+
+func TestCopySavedIndividualizationFileToARS(t *testing.T) {
+	// todo
+	//copySavedIndividualizationFileToARS()
+}
+
+func TestCopyLocallyGeneratedFilesToPatchTool(t *testing.T) {
+	// todo
+	//copyLocallyGeneratedFilesToPatchTool()
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"divekit-cli/cmd"
+	_ "divekit-cli/cmd/patch" // NOTE: Each subcommand must include this import to be acknowledged as a valid subcommand.
 	"fmt"
 	"github.com/apex/log"
 	"os"
