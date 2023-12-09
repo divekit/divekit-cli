c5bdc05e4a5d74bb62312200dfbd504d04c14380
thomasdonst <thomas.donst@gmx.de>
2023-12-09 01:10:57 +0100

    added tests for patch

--- cmd/patch/patch_test.go
+++ cmd/patch/patch_test.go
@@ -1,35 +1,126 @@
 package patch
 
 import (
+	"bytes"
+	"divekit-cli/cmd"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
-func TestRun(t *testing.T) {
-	// todo
-	//patch = &cobra.Command{Use: "root", RunE: run, PreRunE: preRun}
-	//RootCmdFlags(patch)
-	//
-	//err := patch.Execute()
-	//
-	//t.Errorf("error: %v", err)
+func TestPatch(t *testing.T) {
+	testCases := []struct {
+		name      string
+		arguments PatchArguments
+		// repoIds // expected
+	}{
+		{
+			"patch one file",
+			PatchArguments{
+				true,
+				"",
+				"divekit-origin-test-repo",
+				"C:/Users/Thomas/Documents/Praxisprojekt/Git",
+				"",
+				[]string{"$PersonClass$RegistrationUseCases.java"},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			clearARSGeneratedFiles()
+			_, err := executePatch(testCase.arguments)
+
+			assert.NoError(t, err)
+			if testCase.arguments.dryRun {
+				// todo: check if files have been patched
+			} else {
+				// todo: check if files have been patched
+			}
+		})
+	}
 }
 
-func TestPreRun(t *testing.T) {
-	// todo
-	//patch = &cobra.Command{Use: "root", RunE: PreRun}
+func clearARSGeneratedFiles() {
+
 }
 
-func TestDefinePatchFiles(t *testing.T) {
-	// todo
-	//definePatchFiles()
+func executePatch(args PatchArguments) (string, error) {
+	root := createCmd()
+	cmdWithArgs := strings.Split("patch "+args.toString(), " ")
+
+	buffer := new(bytes.Buffer)
+	root.SetOut(buffer)
+	root.SetErr(buffer)
+	root.SetArgs(cmdWithArgs)
+
+	err := root.Execute()
+	output := buffer.String()
+	println(output)
+
+	return output, err
+}
+func createCmd() *cobra.Command {
+	rootCmd := cmd.NewRootCmd()
+	cmd.SetCmdFlags(rootCmd)
+
+	patchCmd := NewPatchCmd()
+	setCmdFlags(patchCmd)
+	rootCmd.AddCommand(patchCmd)
+
+	return rootCmd
 }
 
-func TestCopySavedIndividualizationFileToARS(t *testing.T) {
-	// todo
-	//copySavedIndividualizationFileToARS()
+type PatchArguments struct {
+	dryRun       bool
+	logLevel     string
+	originRepo   string
+	home         string
+	distribution string
+	patchFiles   []string
 }
 
-func TestCopyLocallyGeneratedFilesToPatchTool(t *testing.T) {
-	// todo
-	//copyLocallyGeneratedFilesToPatchTool()
+func (p PatchArguments) toString() string {
+	result := ""
+	if p.dryRun {
+		result += "-0 "
+	}
+	if p.logLevel != "" {
+		result += "-l " + p.logLevel + " "
+	}
+	if p.originRepo != "" {
+		result += "-o " + p.originRepo + " "
+	}
+	if p.home != "" {
+		result += "-m " + p.home + " "
+	}
+	if p.distribution != "" {
+		result += "-d " + p.distribution + " "
+	}
+	if len(p.patchFiles) > 0 {
+		result += strings.Join(p.patchFiles, " ")
+	}
+	return result
 }
+
+//func TestPreRun(t *testing.T) {
+//	// todo
+//	//patch = &cobra.Command{Use: "root", RunE: PreRun}
+//}
+//
+//func TestDefinePatchFiles(t *testing.T) {
+//	// todo
+//	//definePatchFiles()
+//}
+//
+//func TestCopySavedIndividualizationFileToARS(t *testing.T) {
+//	// todo
+//	//copySavedIndividualizationFileToARS()
+//}
+//
+//func TestCopyLocallyGeneratedFilesToPatchTool(t *testing.T) {
+//	// todo
+//	//copyLocallyGeneratedFilesToPatchTool()
+//}
--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"divekit-cli/divekit"
 	"divekit-cli/divekit/origin"
+	"divekit-cli/utils/errorHandling"
 	"divekit-cli/utils/logUtils"
 	"divekit-cli/utils/runner"
 	"github.com/apex/log"
@@ -15,32 +16,39 @@ var (
 	LogLevelFlag       string
 	DivekitHomeFlag    string
 
-	RootCmd = &cobra.Command{
+	RootCmd = NewRootCmd()
+)
+
+func init() {
+	log.Debug("divekit.init()")
+	RootCmd.Version = "0.0.1" // todo: get version from git tag
+	SetCmdFlags(RootCmd)
+}
+
+func NewRootCmd() *cobra.Command {
+	return &cobra.Command{
 		Use:   "divekit",
 		Short: "divekit helps to create and distribute individualized repos for software engineering exercises",
 		Long: `Divekit has been developed at TH Köln by the ArchiLab team (www.archi-lab.io) as
 universal tool to design, individualize, distribute, assess, patch, and evaluate
 realistic software engineering exercises as Git repos.`,
 		PersistentPreRun: persistentPreRun,
-		SilenceErrors:    true,
-		SilenceUsage:     true,
 	}
-)
+}
 
-func init() {
-	log.Debug("divekit.init()")
-	RootCmd.PersistentFlags().BoolVarP(&runner.DryRunFlag, "dry-Run", "0", false,
+func SetCmdFlags(cmd *cobra.Command) {
+	cmd.PersistentFlags().BoolVarP(&runner.DryRunFlag, "dry-Run", "0", false,
 		"just tell what you would do, but don't do it yet")
-	RootCmd.PersistentFlags().StringVarP(&LogLevelFlag, "loglevel", "l", "info",
+	cmd.PersistentFlags().StringVarP(&LogLevelFlag, "loglevel", "l", "info",
 		"log level (warn, info, debug, error)")
-	RootCmd.PersistentFlags().StringVarP(&OriginRepoNameFlag, "originrepo", "o", "",
+	cmd.PersistentFlags().StringVarP(&OriginRepoNameFlag, "originrepo", "o", "",
 		"name of the origin repo to work with")
-	RootCmd.PersistentFlags().StringVarP(&DivekitHomeFlag, "home", "m", "",
+	cmd.PersistentFlags().StringVarP(&DivekitHomeFlag, "home", "m", "",
 		"home directory of all the Divekit repos")
 }
 
 func persistentPreRun(cmd *cobra.Command, args []string) {
-	logUtils.DefineLoggingLevel(LogLevelFlag)
+	errorHandling.OutputAndAbortIfError(logUtils.DefineLoggingLevel(LogLevelFlag), "Could not define log level flag")
 	log.Debug("divekit.persistentPreRun()")
 	divekit.InitDivekitHomeDir(DivekitHomeFlag)
 	origin.InitOriginRepo(OriginRepoNameFlag)
