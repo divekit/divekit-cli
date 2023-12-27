0ccfb507c3490da486e028e4e19bff5bf0d21a3f
thomasdonst <thomas.donst@gmx.de>
2023-12-27 14:13:14 +0100

    only hide progress bar when needed

--- scripts/install_dependencies.ps1
+++ scripts/install_dependencies.ps1
@@ -20,14 +20,13 @@ function install([string]$cmdName, [string]$packageId, [string]$packageManager, 
 function initialize() {
     # install winget if not installed (https://winget.pro/winget-install-powershell/)
     if (-not(commandExists("winget"))) {
-        # temporarily disable progress bar to process faster
-        $ProgressPreference = 'SilentlyContinue'
-
         # prevent "microsoft.ui.xaml.2.7" could not be found
         write-output "Downloading microsoft.ui.xaml.2.7 ..."
+        $ProgressPreference = 'SilentlyContinue'
         invoke-webrequest `
             -uri https://www.nuget.org/api/v2/package/microsoft.ui.xaml/2.7.3 `
             -outfile xaml.zip -useBasicParsing
+        $ProgressPreference = 'Continue'
         new-item -itemtype directory -path xaml
         expand-archive -path xaml.zip -destinationpath xaml
         write-output "Installing microsoft.ui.xaml.2.7 ..."
@@ -37,21 +36,24 @@ function initialize() {
 
         # prevent "microsoft.vclibs.140.00.uwpdesktop" could not be found
         write-output "Downloading microsoft.vclibs.140.00.uwpdesktop ..."
+        $ProgressPreference = 'SilentlyContinue'
         invoke-webrequest `
             -uri https://aka.ms/microsoft.vclibs.x64.14.00.desktop.appx `
             -outfile uwpdesktop.appx -useBasicParsing
+        $ProgressPreference = 'Continue'
         write-output "Installing microsoft.vclibs.140.00.uwpdesktop ..."
         add-appxpackage uwpdesktop.appx
         remove-item uwpdesktop.appx
 
         # get the download URL of the latest winget installer from GitHub
+        $ProgressPreference = 'SilentlyContinue'
         $api_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
         $download_url = $(invoke-restmethod $api_url).assets.browser_download_url |
                 where-object {$_.endsWith(".msixbundle")}
-
         # download the installer
         write-output "Downloading winget ..."
         invoke-webrequest -uri $download_url -outFile winget.msixbundle -useBasicParsing
+        $ProgressPreference = 'Continue'
 
         # install winget
         write-output "Installing winget ..."
@@ -62,9 +64,6 @@ function initialize() {
 
         # refresh $env:Path to use winget
         $Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-        # enable progress bar again
-        $ProgressPreference = 'Continue'
     }
     # install choco if not installed
     if (-not(commandExists("choco"))) {
